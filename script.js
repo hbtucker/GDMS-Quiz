6fcbcdaaaae4ab327c10db16608bb8ae8a3a3214
hbtucker <132301974+hbtucker@users.noreply.github.com>
2024-09-28 17:49:19 +1200

    Update script.js

--- script.js
+++ script.js
@@ -360,7 +360,7 @@ function generatePDF() {
 
     html2canvas(document.getElementById("radarChart")).then(canvas => {
       const imgData = canvas.toDataURL("image/png");
-      const imgWidth = 100;
+      const imgWidth = 150;
       const imgHeight = (canvas.height * imgWidth) / canvas.width;
       doc.addPage();
       doc.addImage(imgData, "PNG", 15, 20, imgWidth, imgHeight);
